ef8bf5b8eec5f2832244132f6e276430a1cbb1ee
AppDevIn <teamprojectlive@gmail.com>
2025-02-20 11:09:19 +0800

    WiFI optional part A

--- CS2040S/problem-set/WIFI-Optional/WiFi/src/WiFi.java
+++ CS2040S/problem-set/WIFI-Optional/WiFi/src/WiFi.java
@@ -6,13 +6,30 @@ class WiFi {
      * Implement your solution here
      */
     public static double computeDistance(int[] houses, int numOfAccessPoints) {
-        return 0.0;
+        double maxDiff = 0;
+        for (int i = 0; i < houses.length-1; i++) {
+            maxDiff = Math.max(maxDiff, Math.abs(houses[i] - houses[i+1]));
+        }
+        while (coverable(houses, numOfAccessPoints, maxDiff/2)) {
+            maxDiff /= 2;
+        }
+
+        return maxDiff;
     }
 
     /**
      * Implement your solution here
      */
     public static boolean coverable(int[] houses, int numOfAccessPoints, double distance) {
-        return false;
+        distance *= 2;
+        double curr = distance + houses[0];
+        for (int i = 0; i < houses.length; i++) {
+            if  (houses[i] > curr) {
+                curr += distance + houses[i];
+                numOfAccessPoints--;
+            }
+        }
+
+        return numOfAccessPoints >= 1;
     }
 }
