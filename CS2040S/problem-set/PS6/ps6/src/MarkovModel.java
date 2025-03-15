89183bdc8ca0b77199fb46c7c54945373d0b7567
AppDevIn <teamprojectlive@gmail.com>
2025-03-15 13:19:38 +0800

    CS2040S PS6 Part A completed

---
+++ CS2040S/problem-set/PS6/ps6/src/MarkovModel.java
@@ -0,0 +1,82 @@
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ * This is the main class for your Markov Model.
+ *
+ * Assume that the text will contain ASCII characters in the range [1,255].
+ * ASCII character 0 (the NULL character) will be treated as a non-character.
+ *
+ * Any such NULL characters in the original text should be ignored.
+ */
+public class MarkovModel {
+
+	// Use this to generate random numbers as needed
+	private Random generator = new Random();
+
+	private int order;
+
+	private HashMap<String, HashMap<Character, Integer>> hashMap;
+
+	// This is a special symbol to indicate no character
+	public static final char NOCHARACTER = (char) 0;
+
+	/**
+	 * Constructor for MarkovModel class.
+	 *
+	 * @param order the number of characters to identify for the Markov Model sequence
+	 * @param seed the seed used by the random number generator
+	 */
+	public MarkovModel(int order, long seed) {
+		// Initialize your class here
+		this.order = order;
+
+		// Initialize the random number generator
+		generator.setSeed(seed);
+
+		//Inti Hash Table
+		hashMap = new HashMap<String, HashMap<Character, Integer>>();
+
+	}
+
+	/**
+	 * Builds the Markov Model based on the specified text string.
+	 */
+	public void initializeText(String text) {
+		int i = 0;
+		while (i + order < text.length()) {
+			String key = text.substring(i, i + order);
+			char value = text.charAt(i + order);
+
+			hashMap.putIfAbsent(key, new HashMap<>());
+			hashMap.get(key).put(value, hashMap.get(key).getOrDefault(value, 0) + 1);
+			hashMap.get(key).put('@', hashMap.get(key).getOrDefault('@', 0) + 1);
+			i++;
+		}
+	}
+
+	/**
+	 * Returns the number of times the specified kgram appeared in the text.
+	 */
+	public int getFrequency(String kgram) {
+		return hashMap.get(kgram).get('@');
+	}
+
+	/**
+	 * Returns the number of times the character c appears immediately after the specified kgram.
+	 */
+	public int getFrequency(String kgram, char c) {
+		return hashMap.get(kgram).getOrDefault(c, 0);
+	}
+
+	/**
+	 * Generates the next character from the Markov Model.
+	 * Return NOCHARACTER if the kgram is not in the table, or if there is no
+	 * valid character following the kgram.
+	 */
+	public char nextCharacter(String kgram) {
+		// See the problem set description for details
+		// on how to make the random selection.
+		return 'a';
+	}
+}
